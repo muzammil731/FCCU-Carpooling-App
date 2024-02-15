4c67d711aa6d78398f25612a3c3599ba07b72931
muzammil731 <110345160+muzammil731@users.noreply.github.com>
2024-02-15 13:57:12 +0000

    Add files via upload

---
+++ TransportService/TransportService/urls.py
@@ -0,0 +1,27 @@
+"""TransportService URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+
+admin.site.site_header = "Transport Service Admin"
+admin.site.site_title = "Transport Service Admin Portal"
+admin.site.index_title = "Welcome to Transport Service Admin Portal"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('jet/', include('jet.urls', 'jet')),
+    path('',include('transport.urls')),
+]
